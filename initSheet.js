848c4dabc2bf3c0cb2a6ef167696ff1bbf21d597
taku22 <takuya.nakashima@mediagene.co.jp>
2021-02-04 12:57:23 +0900

    create initSheet.js

---
+++ initSheet.js
@@ -0,0 +1,76 @@
+function initTriger() {
+  initSheet();
+}
+
+function initSheet() {
+  const targetSheets = callSheets();
+
+  for (let i = 0; targetSheets.length > i; i++) {
+    deleteData(targetSheets[i]);
+  }
+}
+
+function callSheets() {
+  let arr = [],
+    noteKeysSheet = getNoteKeysSheet(),
+    storeAccountsSheet = getStoreAccountsSheet(),
+    byItemList = getByItemList(),
+    purchaseManagementSheet = getPurchaseManagementSheet(),
+    copiedPurchaseManagementSheet = getCopiedPurchaseManagementSheet(),
+    archiveSheets = getArchiveSheet();
+
+  arr.push([noteKeysSheet, 1]);
+  arr.push([storeAccountsSheet, 1]);
+  arr.push([byItemList, 2]);
+  arr.push([purchaseManagementSheet, 3]);
+  arr.push([copiedPurchaseManagementSheet, 3]);
+
+  if(archiveSheets.length > 0) {
+    for(let i = 0; archiveSheets.length > i; i++) {
+      arr.push([archiveSheets[i], 4])
+    }
+  }
+
+  return arr;
+}
+
+function getArchiveSheet() {
+  let ss = SpreadsheetApp.getActiveSpreadsheet(),
+    sheets = ss.getSheets(),
+    sheetName,
+    match = /【/;
+    let archiveSheets = [];
+
+  for (let i = 0; sheets.length > i; i++) {
+    sheetName = sheets[i].getName();
+    if (sheetName.search(match) >= 0) {
+      archiveSheets.push(sheets[i]);
+    }
+  }
+  return archiveSheets;
+}
+
+
+
+function deleteData(sheetObj) {
+  switch (sheetObj[1]) {
+    case 1:
+      sheetObj[0].getDataRange().clearContent();
+      break;
+    case 2:
+      sheetObj[0].getRange(2, 6, 1, 2).clearContent();
+      sheetObj[0].getRange(4, 1, sheetObj[0].getLastRow(), sheetObj[0].getLastColumn()).clearContent();
+      break;
+    case 3:
+      sheetObj[0].getRange(2, 11, sheetObj[0].getLastRow(), 13).clearContent();
+      sheetObj[0].getRange(2, 11, sheetObj[0].getLastRow(), 13).clearNote();
+      break;
+    case 4:
+      SpreadsheetApp.getActiveSpreadsheet().deleteSheet(sheetObj[0]);
+      break; 
+    default:
+      break;
+  }
+
+
+}
